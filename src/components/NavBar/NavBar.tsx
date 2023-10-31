5790f98c0934da6ee926a2b6ab124f21d54fd598
ismaelbella <ismael.bella@oclock.school>
2023-10-31 17:52:59 +0100

    update components navbar with semantic-ui

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { Input, Menu, Dropdown, Image } from 'semantic-ui-react';
 
 import UserProfileMenu from '../UserProfileMenu/UserProfileMenu';
 
@@ -7,26 +9,57 @@ import logo from '../../assets/logo-site.png';
 import './NavBar.scss';
 
 function NavBar() {
+  const logoStyle = {
+    width: '50px', // Largeur souhaitée
+    height: '40px', // Hauteur souhaitée
+  };
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={logo} alt="Logo" />
-      </div>
-      <ul className="navbar-links">
-        <li>
-          <NavLink to="/">Accueil</NavLink>
-        </li>
-        <li>
-          <NavLink to="/game">Jeux</NavLink>
-        </li>
-        <li>
-          <NavLink to="/wiki">Wiki</NavLink>
-        </li>
-      </ul>
-      <div className="navbar-user-profile-menu">
-        <UserProfileMenu />
-      </div>
-    </nav>
+    <Menu secondary>
+      <Image src={logo} style={logoStyle} />
+      <Menu.Item name="Accueil" />
+      <Menu.Item name="Jeux" />
+      <Menu.Item name="Wiki" />
+      <Menu.Menu position="right">
+        <Menu vertical>
+          <Dropdown item text="Menu">
+            <Dropdown.Menu>
+              <Dropdown.Item>Se connecter</Dropdown.Item>
+              <Dropdown.Item>S'inscrire</Dropdown.Item>
+              <Dropdown.Item>Accéder au profil</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu>
+      </Menu.Menu>
+    </Menu>
+
+
+
+
+
+
+
+
+
+
+    // <nav className="navbar">
+    //   <div className="navbar-logo">
+    //     <img src={logo} alt="Logo" />
+    //   </div>
+    //   <ul className="navbar-links">
+    //     <li>
+    //       <NavLink to="/">Accueil</NavLink>
+    //     </li>
+    //     <li>
+    //       <NavLink to="/game">Jeux</NavLink>
+    //     </li>
+    //     <li>
+    //       <NavLink to="/wiki">Wiki</NavLink>
+    //     </li>
+    //   </ul>
+    //   <div className="navbar-user-profile-menu">
+    //     <UserProfileMenu />
+    //   </div>
+    // </nav>
   );
 }
 
