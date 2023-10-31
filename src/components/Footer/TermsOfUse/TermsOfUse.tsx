507a5ccaefedee4b467a3992496f5aaa51184771
Marine-Lautissier <Marine-L@outlook.com>
2023-10-31 10:40:22 +0100

    feat: creation components Footer & Contact & LegalNotice & TermsOfUse (static)

---
+++ src/components/Footer/TermsOfUse/TermsOfUse.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function TermsOfUse() {
+  return (
+    <div className="terms-of-use">
+      <h1>Conditions Générales d'Utilisation (CGU)</h1>
+      Acceptation des Conditions
+En accédant ou en utilisant le site web NatureGame (ci-après dénommé "le Site") ou toute application, contenu ou service connexe, vous acceptez de vous conformer aux présentes Conditions Générales d'Utilisation. Si vous n'acceptez pas ces conditions, veuillez ne pas utiliser le Site.
+
+Utilisation du Site
+2.1. Vous acceptez de n'utiliser le Site que conformément aux lois applicables et aux présentes conditions.
+
+2.2. Vous acceptez de ne pas interférer avec le bon fonctionnement du Site ni d'accéder aux données du Site d'une manière non autorisée.
+
+Compte Utilisateur
+3.1. Si vous créez un compte utilisateur sur le Site, vous êtes responsable de la confidentialité de votre compte et de votre mot de passe. Vous acceptez de nous informer immédiatement de toute utilisation non autorisée de votre compte.
+
+3.2. Nous nous réservons le droit de suspendre ou de résilier votre compte si nous suspectons une utilisation non autorisée ou abusive.
+
+Contenu Utilisateur
+4.1. En soumettant du contenu sur le Site, vous garantissez que vous avez les droits nécessaires pour le faire et que ce contenu est conforme à nos directives et aux lois applicables.
+
+4.2. Nous nous réservons le droit de retirer ou de refuser tout contenu qui enfreint ces conditions.
+
+Propriété Intellectuelle
+5.1. Le contenu du Site, y compris le texte, les images, les vidéos et les marques, est protégé par les lois sur la propriété intellectuelle.
+
+5.2. Vous acceptez de ne pas copier, reproduire, distribuer ou utiliser le contenu du Site sans autorisation écrite.
+
+Limitation de Responsabilité
+6.1. Le Site est fourni "tel quel" et nous ne garantissons pas son accessibilité ou son absence d'erreurs.
+
+6.2. Nous ne sommes pas responsables des dommages directs, indirects, spéciaux, consécutifs ou autres découlant de votre utilisation du Site.
+
+Modification des Conditions
+7.1. Nous nous réservons le droit de modifier ces conditions à tout moment. Les modifications seront publiées sur le Site, et il vous incombe de consulter régulièrement les conditions mises à jour.
+
+Loi Applicable
+8.1. Ces conditions sont régies par la loi [Nom de la loi applicable] et tout litige sera soumis à la juridiction exclusive des tribunaux de [Nom de la juridiction compétente].
+    </div>
+  );
+}
+
+export default TermsOfUse;
